b145562c9d05453c9aaf116f681f4765d5b68fe7
Harsha <sriharsha.jayanthi@amway.com>
2026-02-26 01:22:31 -0500

    Initial Commit!

---
+++ src/main/java/com/pgms/PgmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pgms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgms/PgmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
